34c444058a12d1e0b1dc1cc86ea348b265b1e343
Alex <alexelmtz@outlook.com>
2017-11-11 20:44:19 -0600

    Created login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'admin/index'
+
+  # get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'admin' => 'admin#index'
+  controller :sessions do
+  	get 'login' => :new
+  	post 'login' => :create
+  	delete 'logout' => :destroy
+  end
+
+  root 'sessions#new'
 end
+
+
